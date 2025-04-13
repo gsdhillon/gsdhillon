a4f8371a843ba34dd27d5230868c3d0155085cd5
Gurmeet Singh <gsdhillon@gmail.com>
2025-04-13 20:10:54 +0530

    Create Install vscode and node.js

---
+++ Install vscode and node.js
@@ -0,0 +1,12 @@
+### Install VSCODE on Windows 11
+* Download setup VSCodeUserSetup-x64-1.99.2.exe from https://code.visualstudio.com/download. Run the setup.
+
+
+
+### Install Node.js on Window 11
+* Doawload node-v22.14.0-x64.msi https://nodejs.org/en/download. Run the setup. Check :
+```
+node -v
+npm -v
+```
+
